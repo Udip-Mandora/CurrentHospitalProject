c67acc2e16b3e72e47f4652e65d17f1840d6dbfb
Udip <udipmandora42@gmail.com>
2023-04-02 22:55:45 -0400

    Added CRUD Operations for Donations

---
+++ HospitalManagementSystem/Controllers/DonationsController.cs
@@ -0,0 +1,156 @@
+﻿using HospitalManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace HospitalManagementSystem.Controllers
+{
+    public class DonationsController : Controller
+    {
+        private static readonly HttpClient client;
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+
+        static DonationsController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44316/api/DonationsData/");
+        }
+
+
+        // GET: Donations/List
+        public ActionResult List()
+        {
+            string url = "ListDonations";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            Debug.WriteLine("The response code is ");
+            Debug.WriteLine(response.StatusCode);
+
+            IEnumerable<Donation> donations = response.Content.ReadAsAsync<IEnumerable<Donation>>().Result;
+            Debug.WriteLine(message: "Number of donations recieved: ");
+            Debug.WriteLine(donations.Count());
+
+            return View(donations);
+        }
+
+        // GET: Donations/Details/5
+        public ActionResult Details(int id)
+        {
+            string url = "FindDonation/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            Debug.WriteLine("The response code is ");
+            Debug.WriteLine(response.StatusCode);
+
+            Donation selectedDonation = response.Content.ReadAsAsync<Donation>().Result;
+            Debug.WriteLine("Name of Doner: ");
+            Debug.WriteLine(selectedDonation.firstName);
+
+            return View(selectedDonation);
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        // GET: Donations/New
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        // POST: Donations/Create
+        [HttpPost]
+        public ActionResult Create(Donation donations)
+        {
+            Debug.WriteLine("The jsonpayload is:");
+            //Debug.WriteLine(donations.firstName);
+            //OBJECTIVE: add a new donation into our system using the API
+            //curl -d @donations.json -H "Content-type:application/json" https://localhost:44316/api/DonationsData/AddDonation
+            string url = "AddDonation";
+
+            string jsonpayload = jss.Serialize(donations);
+
+            Debug.WriteLine(jsonpayload);
+
+            HttpContent content = new StringContent(jsonpayload);
+
+            content.Headers.ContentType.MediaType = "application/json";
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
+        }
+
+        // GET: Donations/Edit/5
+        public ActionResult Edit(int id)
+        {
+            string url = "FindDonation/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            Donation selectedDonation = response.Content.ReadAsAsync<Donation>().Result;
+            return View(selectedDonation);
+        }
+
+        // POST: Donations/Update/5
+        [HttpPost]
+        public ActionResult Update(int id, Donation donations)
+        {
+            string url = "UpdateDonation/" + id;
+            string jsonpayload = jss.Serialize(donations);
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            Debug.WriteLine(content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
+        }
+
+        // GET: Donations/DeleteConfirm/5
+        public ActionResult DeleteConfirm(int id)
+        {
+            string url = "FindDonation/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            Donation selectedDonation = response.Content.ReadAsAsync<Donation>().Result;
+            return View(selectedDonation);
+        }
+
+        // POST: Donations/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            string url = "DeleteDonation/" + id;
+            HttpContent content = new StringContent("");
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
+        }
+    }
+}
---
+++ HospitalManagementSystem/Controllers/DonationsDataController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HospitalManagementSystem.Models;
+
+namespace HospitalManagementSystem.Controllers
+{
+    public class DonationsDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        /// <summary>
+        /// List all donations available in the system.
+        /// </summary>
+        /// <returns>
+        /// HEADER: 200 (OK)
+        /// CONTENT: List of donations in the system.
+        /// </returns>
+        /// <example>
+        /// GET: api/DonationsData/ListDonations
+        /// </example>
+        [HttpGet]
+        public IQueryable<Donation> ListDonations()
+        {
+            return db.Donations;
+        }
+
+        /// <summary>
+        /// Find donation in the system.
+        /// </summary>
+        /// <returns>
+        /// HEADER: 200 (OK)
+        /// CONTENT: Find donation in the system.
+        /// </returns>
+        /// <example>
+        /// GET: api/DonationsData/FindDonation/5
+        /// </example>
+        [ResponseType(typeof(Donation))]
+        [HttpGet]
+        public IHttpActionResult FindDonation(int id)
+        {
+            Donation donation = db.Donations.Find(id);
+            if (donation == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(donation);
+        }
+
+        /// <summary>
+        /// Update donations in the system.
+        /// </summary>
+        /// <returns>
+        /// HEADER: 200 (OK)
+        /// CONTENT: Update donations in the system.
+        /// </returns>
+        /// <example>
+        /// PUT: api/DonationsData/UpdateDonation/5
+        /// </example>
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        public IHttpActionResult UpdateDonation(int id, Donation donation)
+        {
+            Debug.WriteLine("I have reached the update donation method.");
+            if (!ModelState.IsValid)
+            {
+                Debug.WriteLine("Model state is invalid");
+                return BadRequest(ModelState);
+            }
+
+            if (id != donation.donationID)
+            {
+                Debug.WriteLine("ID mismatched");
+                return BadRequest();
+            }
+
+            db.Entry(donation).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DonationExists(id))
+                {
+                    Debug.WriteLine("Donation does not exist");
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            Debug.WriteLine("None of the conditions triggered");
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        /// <summary>
+        /// Add donation in the system.
+        /// </summary>
+        /// <returns>
+        /// HEADER: 200 (OK)
+        /// CONTENT: Add donation in the system.
+        /// </returns>
+        /// <example>
+        /// POST: api/DonationsData/AddDonation
+        /// </example>
+        [ResponseType(typeof(Donation))]
+        [HttpPost]
+        public IHttpActionResult AddDonation(Donation donation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Donations.Add(donation);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = donation.donationID }, donation);
+        }
+
+        /// <summary>
+        /// Delete Donation in the system.
+        /// </summary>
+        /// <returns>
+        /// HEADER: 200 (OK)
+        /// CONTENT: Delete donation in the system.
+        /// </returns>
+        /// <example>
+        /// DELETE: api/DonationsData/DeleteDonation/5
+        /// </example>
+        [ResponseType(typeof(Donation))]
+        [HttpPost]
+        public IHttpActionResult DeleteDonation(int id)
+        {
+            Donation donation = db.Donations.Find(id);
+            if (donation == null)
+            {
+                return NotFound();
+            }
+
+            db.Donations.Remove(donation);
+            db.SaveChanges();
+
+            return Ok(donation);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DonationExists(int id)
+        {
+            return db.Donations.Count(e => e.donationID == id) > 0;
+        }
+    }
+}
